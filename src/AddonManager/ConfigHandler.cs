82c635fdd4a19336fc173e5c760b1474f26832e5
Evan Burkey <evan@burkey.co>
2017-06-24 08:52:27 -0700

    Setup plumbing for sqlite

--- src/AddonManager/ConfigHandler.cs
+++ src/AddonManager/ConfigHandler.cs
@@ -1,6 +1,7 @@
 ﻿using IniParser;
 using IniParser.Model;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.IO;
 
 namespace AddonManager
@@ -32,6 +33,15 @@ namespace AddonManager
         public static List<string> GetAddons()
         {
             //const string filepath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AddonManager\\addons.txt";
+
+            // Plumbing for moving datafile from txt to sqlite
+            /*
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=addons.sqlite;Version=3;");
+            m_dbConnection.Open();
+            m_dbConnection.Close();
+            */
+
             const string filepath = "addons.txt";
             List<string> addons = new List<string>();
 
@@ -39,6 +49,8 @@ namespace AddonManager
             foreach (var line in lines)
                 addons.Add(line);
             return addons;
+
+
         }
     }
 }
